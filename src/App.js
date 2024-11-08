d971cdb596ce953b2891707f76ca16a9fb0f10d2
Barbora6 <smetanovabarbora@email.cz>
2024-11-08 14:23:44 +0100

    adding new imports and routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Babovky from'./Pages/Babovky'
+import Muffiny from './Pages/Muffiny'
+import Cookies from './Pages/Cookies'
+import Home from './Pages/Home'
+import Error from './Pages/Error'
+import SharedLayout from './Pages/SharedLayout'
+
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<SharedLayout/>}>
+        <Route index element={<Home/> }/>
+        <Route path='/babovky' element={<Babovky/>}/>
+        <Route path='/muffiny' element={<Muffiny/>}/>
+        <Route path='/cookies' element={<Cookies/>}/>
+        <Route path='*' element={<Error/>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
