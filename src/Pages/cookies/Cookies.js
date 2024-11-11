580f4f87426765fa627f6d9e302f312045dd2d0d
Barbora6 <smetanovabarbora@email.cz>
2024-11-11 13:08:20 +0100

    changes

--- src/Pages/cookies/Cookies.js
+++ src/Pages/cookies/Cookies.js
@@ -1,27 +1,38 @@
 import recepiesCookies from '../../data/data_cookies'
 import OneRecipe from '../OneRecipe'
+import { useState } from 'react'
 
 const Cookies = () => {
+
+const[showRecipe, setShowRecipe] = useState(false)
+const[selectedArticle, setSelectedArticle] = useState(null)
+
+const handleArticleClick = (article) => {
+  setSelectedArticle(article)
+  setShowRecipe(true)
+  console.log(article);
+  }
+
+
   return <section>
      <h2>Vánoční cukroví</h2>
-     <OneRecipe/>
-    
+          
      {
            recepiesCookies.map((oneCook) => {
             const{id, title, image, description1, description2, difficulty, time } = oneCook
     
-            return <div key={id}>
+            return <article key={id} onClick={() => handleArticleClick(oneCook)}>
               <h2>{title}</h2>
               <img src={image} alt="" />
               <p>{description1}</p>
               <p>{description2}</p>
               <p>{difficulty}</p>
               <p>{time}</p>
-            </div>
+            </article>
           })
      }
      
-    
+     <OneRecipe show={showRecipe} article={selectedArticle} onClose={() => setShowRecipe(false)}/>
   </section>
 }
 
--- src/data/data_marbleCake.js
+++ src/data/data_marbleCake.js
@@ -1,5 +1,5 @@
 import image1 from '../images/image1.jpg'
-import image2 from '../images/image2.png'
+import image2 from '../images/image2.jpg'
 import image3 from '../images/image3.jpg'
 import image4 from '../images/image4.jpg'
 import image5 from '../images/image5.jpg'
