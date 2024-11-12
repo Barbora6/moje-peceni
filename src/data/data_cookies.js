5197cdc85fee1c6f5d3fa125ca6d98fe801a563e
Barbora6 <smetanovabarbora@email.cz>
2024-11-12 12:14:05 +0100

    changed the file of data_cookies

--- src/data/data_cookies.js
+++ src/data/data_cookies.js
@@ -5,31 +5,27 @@ import image15 from '../images/image15.jpg'
 const recepiesCookies = [
     {
         id:13,
-        image: image13,
+        image: 'image13.jpg',
         title: 'Lískooříšková kolečka s nutellou',
-        description1: '<p>Obě mouky, cukr a ořechy promíchejte ve větší míse. Přidejte na kousky pokrájené máslo a jemně nastrouhanou pomerančovou kůru. Promíchejte nožem, poté prsty vypracujte drobenku a hmotu spojte v kompaktní těsto. Pracujte rychle, těsto bude mít tendenci měknout. Zabalte ho do fólie a uložte přes noc do ledničky.',
-        description2: 'Na lehce pomoučněné pracovní ploše vyválejte těsto na plát silný asi 5 mm. Vykrajujte tvary, polovině z nich vykrojte i středy, skládejte je na plech vyložený pečicím papírem a pečte v troubě předehřáté na 170 °C, asi 15 minut. Nechte zcela vychladnout, slepujte nutellou, skládejte do krabice a uložte do chladu.',
+        description: '<p>Obě mouky, cukr a ořechy promíchejte ve větší míse. Přidejte na kousky pokrájené máslo a jemně nastrouhanou pomerančovou kůru. Promíchejte nožem, poté prsty vypracujte drobenku a hmotu spojte v kompaktní těsto. Pracujte rychle, těsto bude mít tendenci měknout. Zabalte ho do fólie a uložte přes noc do ledničky.</p><p>Na lehce pomoučněné pracovní ploše vyválejte těsto na plát silný asi 5 mm. Vykrajujte tvary, polovině z nich vykrojte i středy, skládejte je na plech vyložený pečicím papírem a pečte v troubě předehřáté na 170 °C, asi 15 minut. Nechte zcela vychladnout, slepujte nutellou, skládejte do krabice a uložte do chladu.</p>',
         difficulty: 'snadné',
         time: 20,
     },
 
     {
         id:14,
-        image: image14,
+        image: 'image14.jpg',
         title: 'Křehké sněhové vločky',
-        description1: 'Mouku prosejte na vál, přidejte moučkový cukr a promíchejte. Do mouky udělejte důlek, rozklepněte do něj vejce a přidejte na menší kousky nakrájené máslo. Citron důkladně omyjte a kůru ostrouhejte na jemném struhadle. Přidejte pár kapek vanilkové esence. Všechny suroviny na válu pak rukama rychle, ale důkladně prohněťte. Z hotového těsta udělejte hroudu, zabalte ji do fólie a uložte ji alespoň na hodinu, nejlépe však do druhého dne, do chladničky.',
-        description2: 'Odleželé linecké těsto rozválejte na tenký plát a vykrajujte z něj hvězdičky. Dejte je na plech vyložený pečicím papírem a pečte asi 10 minut – dle velikosti. Po upečení nechte krátce vychladnout na plechu a přemístěte je na vál.',
-        description3: 'Na polevu utřete v míse všechny přísady do zhoustnutí, polijte jí zcela vychladlé cukroví a každý kousek ozdobte cukrovými perličkami nebo krystaly cukru dle fantazie.',
+        description: '<p>Mouku prosejte na vál, přidejte moučkový cukr a promíchejte. Do mouky udělejte důlek, rozklepněte do něj vejce a přidejte na menší kousky nakrájené máslo. Citron důkladně omyjte a kůru ostrouhejte na jemném struhadle. Přidejte pár kapek vanilkové esence. Všechny suroviny na válu pak rukama rychle, ale důkladně prohněťte. Z hotového těsta udělejte hroudu, zabalte ji do fólie a uložte ji alespoň na hodinu, nejlépe však do druhého dne, do chladničky.</p><p>Odleželé linecké těsto rozválejte na tenký plát a vykrajujte z něj hvězdičky. Dejte je na plech vyložený pečicím papírem a pečte asi 10 minut – dle velikosti. Po upečení nechte krátce vychladnout na plechu a přemístěte je na vál.</p><p>Na polevu utřete v míse všechny přísady do zhoustnutí, polijte jí zcela vychladlé cukroví a každý kousek ozdobte cukrovými perličkami nebo krystaly cukru dle fantazie.</p>',
         difficulty: 'snadné',
         time: 40,
     },
 
     {
         id:15,
-        image: image15,
+        image: 'image15.jpg',
         title: 'Kokosové vločky',
-        description1: 'Ve větší míse smíchejte mouku s cukrem a kokosem. Přidejte na kousky pokrájené máslo, žloutky a citronovou kůru. Promíchejte nožem, prsty zpracujte na drobenku a poté spojte v hladké těsto. Pracujte rychle a těsto zbytečně nehněťte. Zabalte ho do fólie a nechte odležet v ledničce do druhého dne.',
-        description2: 'Těsto rozválejte na mírně pomoučněné pracovní ploše na placku silnou asi 5 mm. Vykrajujte tvary a skládejte je na plech vyložený pečicím papírem. Cukroví pečte v troubě předehřáté na 170 °C, asi 15 minut. Počkejte, až zcela vychladnou, poté namáčejte v rozpuštěné čokoládě, skládejte na mřížku, nechte lehce zaschnout, posypte kokosem a nechte zcela ztuhnout. Naskládejte do uzavíratelné krabice a skladujte v chladu.',
+        description: '<p>Ve větší míse smíchejte mouku s cukrem a kokosem. Přidejte na kousky pokrájené máslo, žloutky a citronovou kůru. Promíchejte nožem, prsty zpracujte na drobenku a poté spojte v hladké těsto. Pracujte rychle a těsto zbytečně nehněťte. Zabalte ho do fólie a nechte odležet v ledničce do druhého dne.</p><p>Těsto rozválejte na mírně pomoučněné pracovní ploše na placku silnou asi 5 mm. Vykrajujte tvary a skládejte je na plech vyložený pečicím papírem. Cukroví pečte v troubě předehřáté na 170 °C, asi 15 minut. Počkejte, až zcela vychladnou, poté namáčejte v rozpuštěné čokoládě, skládejte na mřížku, nechte lehce zaschnout, posypte kokosem a nechte zcela ztuhnout. Naskládejte do uzavíratelné krabice a skladujte v chladu.</p>',
         difficulty: 'snadné',
         time: 30,
     },
