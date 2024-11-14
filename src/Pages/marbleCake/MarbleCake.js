34e2c2a3555a947429f850afc3c4f70944d9e551
Barbora6 <smetanovabarbora@email.cz>
2024-11-14 10:46:03 +0100

    changed MarbleCake

--- src/Pages/marbleCake/MarbleCake.js
+++ src/Pages/marbleCake/MarbleCake.js
@@ -1,23 +1,33 @@
 import recepiesMarbleCake from '../../data/data_marbleCake'
+import { useState } from 'react'
 
 const MarbleCake = () => {
+
+const[showRecipe, setShowRecipe] = useState(false)
+const[selectedArticle, setSelectedArticle] = useState(null)
+
+const handleArticleClick = (article) => {
+  setSelectedArticle(article)
+  setShowRecipe(true)
+  }
+
 
   return <section>
     <h2>Bábovky</h2>
 
     {
-        recepiesMarbleCake.map((oneRecept) => {
-            const{id, image, title, description1, description2, description3, difficulty, time} = oneRecept
+        recepiesMarbleCake.map((oneRecipe) => {
+            const{id, image, title, description, difficulty, time} = oneRecipe
             
-        return <div key={id}>
+            return <article key={id} onClick={() => handleArticleClick(oneRecipe)}>
             <h2>{title}</h2>
             <img src={`pictures/${image}`} alt="" />
-            <div dangerouslySetInnerHTML={{__html:description1}}/>
-            <p>{description2}</p>
-            <p>{description3}</p>
+            <div dangerouslySetInnerHTML={{__html:description}}/>
+            <p>{description}</p>
             <p>{difficulty}</p>
             <p>{time}</p>
-        </div>
+            <a href={`/OneRecipe/${id}`}>Detail:</a>
+          </article>
         })
 
     }
