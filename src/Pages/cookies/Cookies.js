74ae30ed4557f5e15e9135303a424d023f1390d8
Barbora6 <smetanovabarbora@email.cz>
2024-11-14 21:49:30 +0100

    changed muffiny and cookies

--- src/Pages/cookies/Cookies.js
+++ src/Pages/cookies/Cookies.js
@@ -13,24 +13,24 @@ const handleArticleClick = (article) => {
   console.log(article);
   }
 
-  return <section>
+  return <Box Width='2500px' display='flex' justifyContent='center' alignItems='center' flexWrap='wrap'>
 
      {
            recepiesCookies.map((oneCook) => {
             const{id, title, image} = oneCook
     
             return <article key={id} onClick={() => handleArticleClick(oneCook)}>
-             <Box width='300px'>
+             <Box width='280px' padding='8px'>
                 <Card>
                   <CardContent>
-                  <Typography gutterBottom variant='h5' component='div'>{title}</Typography>
-                        <CardMedia
+                  <Typography gutterBottom variant='h6' component='div'>{title}</Typography>
+                  <CardMedia
                           component='img'
-                          height='140'
-                          width='250px'
+                          height='240'
+                          width='350'
                           image= {`pictures/${image}`}>
-                          </CardMedia>
-                        {/* <Typography variant='body2' color='text.secondary'><div dangerouslySetInnerHTML={{__html:description}}/></Typography> */}
+                  </CardMedia>
+                           {/* <Typography variant='body2' color='text.secondary'><div dangerouslySetInnerHTML={{__html:description}}/></Typography> */}
                           {/* <Typography variant='body2' color='text.secondary'>Čas přípravy: {time} minut</Typography> */}
                           <CardActions>
                           <a href={`/OneRecipe/${id}`}>Detail:</a>
@@ -38,12 +38,11 @@ const handleArticleClick = (article) => {
                 </CardContent>
                 </Card> 
               </Box>
-              
             </article>
           })
      }
   
-  </section>
+  </Box>
 }
 
 export default Cookies
