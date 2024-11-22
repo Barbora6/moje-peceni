2ebb2cfbb35cbe88afb8d75eb78d37bfb5a968ef
Barbora6 <smetanovabarbora@email.cz>
2024-11-22 17:16:27 +0100

    changes slider text

--- src/Pages/home/Slide/SlideItem.js
+++ src/Pages/home/Slide/SlideItem.js
@@ -12,7 +12,7 @@ const SlideItem = ({image, title, paragraph}) => {
                 loading='lazy' />
       </div>
       <Box sx={{position: 'absolute', top:'50%',left: '50%', transform: 'translate(-50%,-50%)', color:'white', zIndex:2}}>
-        <Typography variant="h4">{title}</Typography>
+        <Typography variant="h2" fontSize='120px' fontWeight={400} letterSpacing={9}>{title}</Typography>
           <Typography>{paragraph}</Typography>
       </Box>
   </Box>
