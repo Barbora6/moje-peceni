d37d6aae1162663db9ba9bc7d8ddc136d1213915
Barbora6 <smetanovabarbora@email.cz>
2024-11-22 20:27:13 +0100

    icons

--- src/Pages/home/Slide/SlideItem.js
+++ src/Pages/home/Slide/SlideItem.js
@@ -13,7 +13,7 @@ const SlideItem = ({image, title, paragraph}) => {
       </div>
       <Box sx={{position: 'absolute', top:'50%', left: '50%', transform: 'translate(-50%,-50%)', color:'white', zIndex:2}}>
         <Typography variant="h2" fontSize='120px' fontWeight={400} letterSpacing={9}>{title}</Typography>
-          <Typography>{paragraph}</Typography>
+          {/* <Typography>{paragraph}</Typography> */}
       </Box>
   </Box>
 
