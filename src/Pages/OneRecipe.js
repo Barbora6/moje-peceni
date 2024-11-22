bd7617b01cbb1cefa49524188a796a82e134b666
Barbora6 <smetanovabarbora@email.cz>
2024-11-22 20:27:13 +0100

    icons

--- src/Pages/OneRecipe.js
+++ src/Pages/OneRecipe.js
@@ -1,7 +1,9 @@
-import { Container, Card, CardContent, CardMedia, Typography} from '@mui/material';
+import { Container, Card, CardContent, CardMedia, Typography, Stack} from '@mui/material';
 import {recepies} from '../data/data_recepies'
 import {useParams } from "react-router-dom";
 import { Resource } from './Resource';
+import { GiCook } from "react-icons/gi";
+import { FaClock } from "react-icons/fa6";
 
 
 export const OneRecipe = () => {
@@ -15,6 +17,7 @@ export const OneRecipe = () => {
 
   return <Container maxWidth='lg'>
           <Card key={id}>
+          <Typography gutterBottom variant="h5" component="div" spacing={4} pt={4}>{title}</Typography>
           <CardMedia component='img' width='100%'sx={{ Maxheight: '100%' }} src={`/pictures/${image}`}></CardMedia>
           {/* Suroviny: */}
           <CardContent>
@@ -22,10 +25,13 @@ export const OneRecipe = () => {
           <Typography variant="body2" sx={{ color: 'text.secondary'}}>
             <Resource/>
           </Typography>
-            <Typography gutterBottom variant="h5" component="div">{title}</Typography>
+          <Typography variant='h5'>Popis:</Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary'}}><div dangerouslySetInnerHTML={{__html:description}}/>
             </Typography>
-            <Typography variant="body2" sx={{ color: 'text.secondary'}}>Obtížnost:{difficulty} {time} min.</Typography>
+            <Stack display='flex' flexDirection='row'>
+            <Typography><GiCook fontSize='24px'/>obtížnost:{difficulty}</Typography>
+            <Typography><FaClock m={2}/>doba přípravy: {time} min.</Typography>
+            </Stack>
           </CardContent>
           </Card>
    </Container>
